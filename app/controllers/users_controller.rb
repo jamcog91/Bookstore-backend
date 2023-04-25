38c1532348a89439b00504ccbb3e727ef0837878
James Cogan <james.m.cogan@gmail.com>
2023-04-25 16:49:41 -0400

    user controler basic methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    def update
+        user = User.find(params[:id])
+        render json: user, status: :accepted
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head :no_content
+        render json: []
+    end
+
+    private
+
+    def user_params
+        perams.permit(:first_name, :last_name, :email, :password, :handle, :avatar)
+    end
 end
